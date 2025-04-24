e9b12cf4d19dc48b5a5d6f1695f65ba45b55ee90
hencci <hencci20@gmail.com>
2025-04-24 06:16:16 +0100

    Select DOM and save in variables

---
+++ Image-Carousel/carousel.js
@@ -0,0 +1,7 @@
+const track = document.getElementById('carouselTrack');
+const slides = document.querySelectorAll('.carousel-slide');
+const prevBtn = document.getElementById('prevBtn');
+const nextBtn = document.getElementById('nextBtn');
+const dotsContainer = document.getElementById('dots');
+let currentIndex = 0;
+let interval;
